3c1fd6dd8b4f188cfd06ad006643c7194f43805b
JonathanPJuhl <cph-jj450@cphbusiness.dk>
2021-04-26 14:51:25 +0200

    changes back and forth2

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
-const URL = "http://localhost:8080/tomcat/ca3/api/";
-//const URL = "/tomcat/ca3/api/"
+//const URL = "http://localhost:8080/tomcat/ca3/api/";
+const URL = "/tomcat/ca3/api/"
 
 const adminURL = URL + "info/admin";
 const userURL = URL + "info/user";
