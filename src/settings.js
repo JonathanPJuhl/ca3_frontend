fa3aa3fbf552a4a4259479e158069066f69364f9
JonathanPJuhl <cph-jj450@cphbusiness.dk>
2021-04-26 09:45:47 +0200

    changing repo

---
+++ src/settings.js
@@ -0,0 +1,3 @@
+const amdinURL = "http://localhost:8080/ca3/api/info/admin";
+const userURL = "http://localhost:8080/ca3/api/info/user";
+const fiveThingsURL = "http://localhost:8080/ca3/api/5things/teachersSolution";
